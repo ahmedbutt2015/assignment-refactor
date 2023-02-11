7420e4e163446555d0bd23828754aeea38fe5940
Ahmed Butt <imahmedthebutt@gmail.com>
2023-02-11 17:06:48 +0500

    made neccesory interfaces in shop-app

--- src/shop-app.tsx
+++ src/shop-app.tsx
@@ -10,10 +10,24 @@ import img1 from "./images/img1.png";
 import img2 from "./images/img2.png";
 import styles from "./shopApp.module.css";
 
-export class ShopApp extends React.Component<
-  {},
-  { products: any[]; isOpen: boolean; isShowingMessage: boolean; message: string; numFavorites: number; prodCount: number }
-> {
+interface Product {
+  title: string;
+  description: string;
+  price: string;
+  isFavorite: boolean;
+}
+
+interface ShopAppState {
+  products: Product[];
+  isOpen: boolean;
+  isShowingMessage: boolean;
+  message: string;
+  numFavorites: number;
+  prodCount: number;
+}
+
+
+export class ShopApp extends React.Component<{}, ShopAppState> {
   constructor(props: any) {
     super(props);
 
